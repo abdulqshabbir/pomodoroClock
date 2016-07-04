d4bdd0121f012c5133b99aa8b5f702a33e054e2d
Abdul Shabbir <abdulqshabbir@gmail.comgit config --global user.email abdulqshabbir@gmail.com>
2016-07-04 19:08:09 -0400

    First Commit

---
+++ Gruntfile.js
@@ -0,0 +1,93 @@
+module.exports = function(grunt) {
+require('load-grunt-tasks')(grunt);
+
+    //All configuration goes here
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+            jshint: {
+                all: ['Gruntfile.js', 'js/app_ES5.js']
+            },
+            concat: {
+                sass: {
+                    src: 'css/**.sass',
+                    dest: 'dest/styles.sass',
+                },
+                js: {
+                    src: 'js/**.js',
+                    dest: 'dest/app.js',
+                },
+            },
+            babel: {
+		        options: {
+			        presets: ['babel-preset-es2015'],
+		        },
+        	    dist: {
+    			    files: {
+    	                'dest/app_ES5.js': 'dest/app.js'
+    			    },
+        	    }
+	        },
+
+            sass: {
+            	dist: {
+        			files: {
+        				'dest/styles.css': 'dest/styles.sass'
+        			},
+        		},
+        	},
+            autoprefixer: {
+                css: {
+                    src: 'dest/styles.css',
+                    dest: 'dest/styles_autoprefixed.css',
+                },
+            },
+            watch: {
+                js: {
+                    files: 'js/*.js',
+                    tasks: ['jshint', 'concat', 'babel', 'jasmine'],
+                    options: {
+                        spawn: false,
+                    },
+                },
+                sass: {
+                    files: 'css/*.sass',
+                    tasks: ['concat', 'sass', 'autoprefixer'],
+                },
+                karma: {
+                    files: ['js/*.js', 'jquery.min.js'],
+                }
+            },
+            karma: {
+                unit: {
+                    files: [{ src: ['assets/jquery.min.js'], served: true, included: false }],
+                    singleRun: true,
+                    logLevel: 'ERROR',
+                    background: true,
+                    options: {
+                        files: ['test/**/*.js'],
+                        tasks: ['karma:unit:run']
+                    }
+                }
+            },
+            jasmine: {
+                pivotal: {
+                    src: 'js/*.js',
+                        options: {
+                            specs: 'js/first.js',
+                            outfile: 'dest/_SpecRunner.html',
+                            keepRunner: 'true'
+                    }
+                }
+            }
+    });
+    //Where we tell Grunt we plan to use this plug-in.
+        grunt.loadNpmTasks('grunt-contrib-concat');
+        grunt.loadNpmTasks('grunt-contrib-uglify');
+        grunt.loadNpmTasks('grunt-autoprefixer');
+        grunt.loadNpmTasks('grunt-contrib-watch');
+        grunt.loadNpmTasks('grunt-karma');
+        grunt.loadNpmTasks('grunt-contrib-jasmine');
+    //Where we tell Grunt what to do when we type "grunt" into the terminal.
+        grunt.registerTask('default', ['concat', 'babel', 'jshint', 'sass', 'autoprefixer', 'jasmine']);
+        grunt.registerTask('grunt watch');
+};
---
+++ dest/app.js
@@ -0,0 +1,169 @@
+$(document).ready(function() {
+	$("#startTimerButton").on("click", function startButtonHandler() {
+		myTimer.startTimer();
+	});
+
+	$("#pauseTimerButton").on("click", function pauseButtonHandler() {
+		myTimer.pauseTimer();
+	});
+
+	$("#stopTimerButton").on("click", function stopButtonHandler() {
+		myTimer.stopTimer();
+	});
+
+	$("#incrementTimer").on("click", function incrementTimerHandler() {
+		myTimer.incrementTimeByOneMinute();
+	});
+
+	$("#decrementTimer").on("click", function decrementTimerHandler() {
+		myTimer.decrementTimeByOneMinute();
+	});
+
+	var myTimer = {
+		isRunning: false,
+		intervalID: null,
+		minutes: minutes,
+		seconds: seconds,
+		decrementTime: decrementTimer,
+		startTimer: startTimer,
+		pauseTimer: pauseTimer,
+		stopTimer: stopTimer,
+		incrementTimeByOneMinute: incrementTimeByOneMinute,
+		decrementTimeByOneMinute: decrementTimeByOneMinute,
+		totalTimeInSeconds: totalTimeInSeconds,
+	};
+
+	function minutes() {
+		var minutes = $("#minutes").html();
+		return minutes;
+	}
+
+	function seconds() {
+		var seconds = $("#seconds").html();
+		return seconds;
+	}
+
+	function startTimer() {
+		myTimer.isRunning = true;
+		myTimer.intervalID = setInterval(decrementTimer, 1000);
+	}
+
+	function decrementTimer() {
+		var seconds = myTimer.seconds();
+		var minutes = myTimer.minutes();
+
+		if(myTimer.isRunning && seconds >=1) {
+			seconds = seconds - 1;
+			$("#seconds").html(seconds);
+
+		} else if(myTimer.isRunning && seconds <= 0 && minutes >= 0) {
+			decrementMinutes();
+
+		} else if(myTimer.isRunning && seconds <=0 && minutes <=0) {
+			timerIsOver();
+		}
+	}
+
+	function decrementMinutes() {
+		var minutes = myTimer.minutes();
+		var seconds = myTimer.seconds();
+		minutes = minutes - 1;
+		$("#seconds").html(59);
+		$("#minutes").html(minutes);
+	}
+
+	function timerIsOver() {
+		$("#seconds").html("your session is over!");
+		$("#minutes").html("your session is over!");
+	}
+
+	function pauseTimer() {
+		var seconds = myTimer.seconds();
+		var minutes = myTimer.minutes();
+		myTimer.isRunning = false;
+		clearInterval(myTimer.intervalID);
+		myTimer.intervalID = null;
+	}
+
+	function stopTimer() {
+		myTimer.isRunning = false;
+		clearInterval(myTimer.intervalID);
+		myTimer.intervalID = null;
+		$("#seconds").html(0);
+		$("#minutes").html(25);
+	}
+
+	function incrementTimeByOneMinute() {
+		var minutes = Number(myTimer.minutes());
+		if (!myTimer.isRunning) {
+			minutes = minutes + 1;
+			$("#minutes").html(minutes);
+		}
+	}
+
+	function decrementTimeByOneMinute() {
+		var minutes = Number(myTimer.minutes());
+		if (!myTimer.isRunning) {
+			minutes = minutes - 1;
+			$("#minutes").html(minutes);
+		}
+	}
+
+	function totalTimeInSeconds() {
+		return Number(myTimer.minutes()*60) + Number(myTimer.seconds());
+	}
+
+
+	var canvas = document.getElementById('myCanvas');
+	canvas.style.position = 'absolute';
+	canvas.style.border = '1px solid black';
+	canvas.style.top = '50px';
+	canvas.style.left = '50px';
+	var context = canvas.getContext('2d');
+	var raf;
+
+	var pomodoroTimer = {
+		positionX: canvas.width/2,
+		positionY: canvas.height/2,
+		radius: 50,
+		startAngle: -0.5*Math.PI,
+		endAngle: -0.5*Math.PI,
+		draw: draw,
+		animate: animate,
+	};
+
+	function draw() {
+		context.beginPath();
+		context.arc(this.positionX, this.positionY, this.radius, this.startAngle, this.endAngle, false);
+		context.lineWidth = 10;
+		context.stroke();
+	}
+
+	function animate() {
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		pomodoroTimer.draw();
+		pomodoroTimer.endAngle = pomodoroTimer.endAngle + (0.1*Math.PI);
+		raf = requestAnimationFrame(animate);
+	}
+	pomodoroTimer.draw();
+
+	if (myTimer.isRunning) {
+		pomodoroTimer.animate();
+	}
+	function hello() {
+		return 'hello';
+	}
+
+	describe("test suite", function() {
+		it('expect to pass', function() {
+			expect(true).toBe(true);
+		});
+	});
+
+	function secondHello() {
+		return 'second hello !';
+	}
+
+});
+
+
